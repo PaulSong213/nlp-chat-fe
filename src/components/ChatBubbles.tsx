0046ef4d30bf4d86ec28883a32480883d3f952ee
John Paul Songalia <paul.songalia@edufied.edu.vn>
2023-08-22 22:12:14 +0800

    Add send chat feature

--- src/components/ChatBubbles.tsx
+++ src/components/ChatBubbles.tsx
@@ -1,3 +1,5 @@
+import ChatTypeLoader from "../components/ChatTypeLoader";
+
 interface Chat {
   isFromBot: boolean;
   message: string;
@@ -5,23 +7,24 @@ interface Chat {
 
 interface ChatBubblesProps {
   chats: Chat[];
+  isBotTyping: boolean;
 }
 
-const ChatBubbles: React.FC<ChatBubblesProps> = ({ chats }) => {
+const ChatBubbles: React.FC<ChatBubblesProps> = ({ chats, isBotTyping }) => {
   return (
-    <div className="flex flex-col-reverse h-full overflow-x-hidden mb-4">
+    <div className="flex flex-col justify-end mt-auto min-h-full pb-3 space-y-2">
       {chats.map((chat, index) => (
         <div
           key={index}
-          className={`flex w-3/4 mt-5 shadow-sm ${
-            chat.isFromBot ? "space-x-2" : "self-end"
+          className={`flex w-3/4 mt-5  ${
+            chat.isFromBot ? "space-x-2 " : "self-end justify-end"
           }`}
         >
           {chat.isFromBot && (
             <img className="w-10 h-10" src="/logo.png" alt="Logo" />
           )}
           <p
-            className={` p-4 rounded-lg break-all ${
+            className={` p-4 rounded-lg break-all shadow-sm ${
               chat.isFromBot
                 ? "bg-slate-700 text-slate-50"
                 : "bg-slate-200 text-slate-800 "
@@ -31,6 +34,7 @@ const ChatBubbles: React.FC<ChatBubblesProps> = ({ chats }) => {
           </p>
         </div>
       ))}
+      <ChatTypeLoader isVisible={isBotTyping} />
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,45 +1,76 @@
 import { Send } from "react-ionicons";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import ChatBubbles from "../components/ChatBubbles";
-import ChatTypeLoader from "../components/ChatTypeLoader";
+
 interface CounterProps {}
 
 const Home: React.FC<CounterProps> = () => {
-  // state of array chats
-  const [chats] = useState([
+  const [userChat, setUserChat] = useState("");
+  const [chats, setChats] = useState([
     {
       isFromBot: true,
-      message:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur mollitia esse, quos at neque et perferendis repudiandae necessitatibus architecto vitae veniam consectetur tempore! Quis, dolorum fugit. Nesciunt repellat nulla minima!",
+      message: "First!",
     },
     {
       isFromBot: false,
-      message:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur mollitia esse, quos at neque et perferendis repudiandae necessitatibus architecto vitae veniam consectetur tempore! Quis, dolorum fugit. Nesciunt repellat nulla minima!",
+      message: "second",
     },
   ]);
 
-  // state of loader while bot is typing
+  // Ref to the chat messages container
+  const chatMessagesRef = useRef<HTMLDivElement | null>(null);
+
+  const sendChat = (chat: string, isFromBot: boolean) => {
+    if (!chat) return;
+    const newChat = {
+      isFromBot: isFromBot,
+      message: chat,
+    };
+
+    setChats([...chats, newChat]);
+    setUserChat("");
+    setIsBotTyping(!isBotTyping);
+  };
+
   const [isBotTyping, setIsBotTyping] = useState(false);
+
+  // Auto-scroll behavior
+  useEffect(() => {
+    if (chatMessagesRef.current) {
+      chatMessagesRef.current.scrollTop = chatMessagesRef.current.scrollHeight;
+    }
+  }, [chats]);
 
   return (
     <div className="flex h-screen antialiased text-gray-800">
       <div className="flex flex-row h-full w-full overflow-x-hidden">
         <div className="flex flex-col flex-auto h-full p-6">
-          <div className="flex flex-col flex-auto flex-shrink-0 rounded-2xl bg-gray-100 h-full p-4">
+          <div className="flex flex-col justify-end flex-auto flex-shrink-0 rounded-2xl bg-gray-100 h-full pl-3 py-3 pr-1 overflow-hidden">
             {/* Chat Messages */}
-            <ChatBubbles chats={chats} />
-            <ChatTypeLoader isVisible={isBotTyping} />
-            <div className="flex space-x-1 items-stretch justify-between ">
-              {/* Chat Input */}
+            <div
+              ref={chatMessagesRef}
+              className="overflow-y-auto p-2 flex-grow"
+            >
+              <ChatBubbles chats={chats} isBotTyping={isBotTyping} />
+            </div>
+
+            {/* Chat Input */}
+            <div className="flex space-x-1 items-stretch justify-between">
               <input
+                value={userChat}
+                onChange={(e) => {
+                  setUserChat(e.target.value);
+                }}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") sendChat(userChat, false);
+                }}
                 type="text"
-                className="z-20 flex flex-row items-center h-16 rounded-xl bg-white w-full px-4 shadow outline-none"
+                className="z-20 flex flex-row items-center h-14 rounded-xl bg-white w-full px-4 shadow outline-none"
               />
-              {/* Sidebar Close button */}
+              {/* Send button */}
               <button
                 onClick={() => {
-                  setIsBotTyping(!isBotTyping);
+                  sendChat(userChat, false);
                 }}
                 type="button"
                 className="p-2 bg-slate-800 text-sm rounded-lg hover:bg-slate-900 focus:outline-none focus:ring-2 focus:ring-gray-200 flex items-center justify-center w-16"
