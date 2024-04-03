2528e2b433856b913c15c552c8a7f54f9be490e9
John Paul Songalia <68486657+PaulSong213@users.noreply.github.com>
2024-04-03 11:21:23 +0800

    Update API base URL

--- src/api.tsx
+++ src/api.tsx
@@ -2,8 +2,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://black-sand-3f09b3a9e48347e780a9c8abd04add48.azurewebsites.net/', // Replace with your API's base URL
+    // baseURL: 'https://black-sand-3f09b3a9e48347e780a9c8abd04add48.azurewebsites.net/', // Replace with your API's base URL
     //baseURL: 'http://127.0.0.1:8000', // Replace with your API's base URL
+    baseURL: 'https://ashy-bay-5331ffa4d6654851ba4c011600c4f97d.azurewebsites.net/', // Replace with your API's base URL
 });
 
 // Example API function
